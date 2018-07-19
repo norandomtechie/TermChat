027f3ad75a5134080806b308ab624a7564e2ffc4
norandomtechie <norandomtechie@users.noreply.github.com>
2018-07-19 16:29:21 -0400

    Error fixes

--- termchat_client.py
+++ termchat_client.py
@@ -1,6 +1,55 @@
 # !/usr/bin/python
-import os, sys, getpass
 from Crypto.Cipher import AES
+import os, sys, getpass, Crypto.Random, hashlib
+
+###########################################################################################
+### https://stackoverflow.com/questions/6425131/encrypt-decrypt-data-in-python-with-salt
+
+# salt size in bytes
+SALT_SIZE = 16
+# number of iterations in the key generation
+NUMBER_OF_ITERATIONS = 25
+# the size multiple required for AES
+AES_MULTIPLE = 16
+
+def generate_key(password, salt, iterations):
+    assert iterations > 0
+    key = password + salt
+    for i in range(iterations):
+        key = hashlib.sha256(key).digest()
+    return key
+
+def pad_text(text, multiple):
+    extra_bytes = len(text) % multiple
+    padding_size = multiple - extra_bytes
+    padding = chr(padding_size) * padding_size
+    padded_text = text + padding
+    return padded_text
+
+def unpad_text(padded_text):
+    padding_size = ord(padded_text[-1])
+    text = padded_text[:-padding_size]
+    return text
+
+def encrypt(plaintext, password):
+    salt = Crypto.Random.get_random_bytes(SALT_SIZE)
+    key = generate_key(password, salt, NUMBER_OF_ITERATIONS)
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded_plaintext = pad_text(plaintext, AES_MULTIPLE)
+    ciphertext = cipher.encrypt(padded_plaintext)
+    ciphertext_with_salt = salt + ciphertext
+    return ciphertext_with_salt
+
+def decrypt(ciphertext, password):
+    salt = ciphertext[0:SALT_SIZE]
+    ciphertext_sans_salt = ciphertext[SALT_SIZE:]
+    key = generate_key(password, salt, NUMBER_OF_ITERATIONS)
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded_plaintext = cipher.decrypt(ciphertext_sans_salt)
+    plaintext = unpad_text(padded_plaintext)
+    return plaintext
+
+###########################################################################################
 
 def new_user (user_name):
     # This function is called if the user wishes to create a new profile.
@@ -30,6 +79,8 @@ def start_conversation (user_name, profile_data):
     recv_user = raw_input ("\nTo whom do you wish to send a message? ")
     search_string = '#### To ' + recv_user + ' ####'
     split_data = profile_data.split ("\n")
+
+    print (profile_data)
 
     # If the user exists, display past messages, otherwise
     # add an entry to the profile_data text string.
@@ -84,10 +135,17 @@ def start_conversation (user_name, profile_data):
 
     return profile_data;
 
+def open_profile (passphrase, file):
+
+    profile_data = decrypt (file.read(), passphrase)
+    profile_data =
+    file.close()
+    return profile_data;
+
 def save_profile (passphrase, profile_data):
     # This function encrypts the final text buffer and saves it to the file
-
-    file.write (profile_data)
+    file.truncate(0)
+    file.write (encrypt (profile_data, passphrase))
     return;
 
 ############################# START OF SCRIPT #############################
@@ -95,12 +153,12 @@ def save_profile (passphrase, profile_data):
 print ('\n########################################################')
 print ("Welcome to TermChat! Developed for a more secure world. \n")
 user_name = raw_input ("Enter username: ")
+ch = 'X'
 
 if os.path.isfile (user_name + '.dat') == True:
     passphrase, file = existing_user (user_name)
-    profile_data = file.read()
+    profile_data = open_profile (passphrase, file)
     file.close()
-    file = open (user_name + '.dat', "w")
 else:
     ch = raw_input ("This user does not exist! Create a new ID? (Y/N): ")
     if ch == 'Y':
@@ -114,12 +172,18 @@ else:
 # if profile_data:
 #     print ('\n' + profile_data)
 
-while raw_input ("Do you wish to send a message? (Y/N): ") == 'Y':
+login = raw_input ("Do you wish to send a message? (Y/N): ")
+if login == 'Y':
+    file = open (user_name + '.dat', "w")
+
+while login == 'Y':
     profile_data = start_conversation (user_name, profile_data)
     save_profile (passphrase, profile_data)
+    login = raw_input ("Do you wish to send another message? (Y/N): ")
     pass
 
-else:
-    print ("Exiting...")
-    print ('########################################################\n')
-    exit()
+file.close()
+
+print ("Exiting...")
+print ('########################################################\n')
+exit()
